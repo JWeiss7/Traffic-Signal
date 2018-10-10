547660a6a3450f8109d0d1ab0c7bbc7257b804f0
JWeiss7 <42100778+JWeiss7@users.noreply.github.com>
2018-10-09 22:30:25 -0400

    Added Files

---
+++ trafficSignal.js
@@ -0,0 +1,28 @@
+function changeRed() {
+    document.getElementById("redLight").style.background = "red";
+    document.getElementById("yellowLight").style.background = "black";
+    document.getElementById("greenLight").style.background = "black";
+
+}
+
+function changeYellow() {
+    document.getElementById("yellowLight").style.background = "yellow";
+    document.getElementById("redLight").style.background = "black";
+    document.getElementById("greenLight").style.background = "black";
+
+}
+
+function changeGreen() {
+    document.getElementById("yellowLight").style.background = "black";
+    document.getElementById("redLight").style.background = "black";
+    document.getElementById("greenLight").style.background = "green";
+
+}
+
+function allLights() {
+    document.getElementById("redLight").style.background = "red";
+    document.getElementById("yellowLight").style.background = "yellow";
+    document.getElementById("greenLight").style.background = "green";
+
+}
+
